11d2f54a742e4b0562a260718ad8cd34923f8230
fernando lima <fernando.palhares.lima@gmail.com>
2021-11-11 23:57:52 -0300

    [ADD] Consulta no Banco de Dados

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,19 +1,25 @@
 #importando as bibliotecas FLASK
 from flask import Flask, render_template, request
 
+from app.models.campaignset import Campaign_Set
+from app.models.campaign import Campaign
+from app.models.campaignobjective import Campaign_Objective
+
 #importando biblioteca para captura de log
 from datetime import datetime
 
-from app import app
+from app import app, db
+
 
 @app.route('/index')
 @app.route('/')
-
 def index():
-    return render_template('index.html')
+    campaignsets = Campaign_Set.query.order_by(Campaign_Set.campaign_set_id).all()
+    return render_template('index.html', campsets=campaignsets)
 
 
-@app.route('/campaignset/<campset_id>')
-def campset(campset_id):
-    campaign = ['alcance', 'tráfego']
-    return render_template('campaignset.html', campset_id=campset_id, campaign=campaign) 
+@app.route('/campaignset/<campaignset_id>')
+def campaignset(campaignset_id):
+    campaignset = Campaign_Set.query.filter_by(campaign_set_id=campaignset_id)
+    campaigns = Campaign.query.filter_by(campaign_set_id=campaignset_id).all()
+    return render_template('campaignset.html', campaignsets=campaignset, campaigns=campaigns)
