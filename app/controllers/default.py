048858834bdfa7677848c57efa46e3c0b8d2051f
fernando lima <fernando.palhares.lima@gmail.com>
2021-11-12 22:11:46 -0300

    [DEV] Criação de Branch Dev

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,9 +1,13 @@
 #importando as bibliotecas FLASK
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, flash, redirect
+import os, datetime
+
+from werkzeug.exceptions import abort
 
 from app.models.campaignset import Campaign_Set
 from app.models.campaign import Campaign
 from app.models.campaignobjective import Campaign_Objective
+from app.models.adset import Ad_Set
 
 #importando biblioteca para captura de log
 from datetime import datetime
@@ -14,12 +18,22 @@ from app import app, db
 @app.route('/index')
 @app.route('/')
 def index():
-    campaignsets = Campaign_Set.query.order_by(Campaign_Set.campaign_set_id).all()
+    campaignsets = Campaign_Set.query.all()
     return render_template('index.html', campsets=campaignsets)
 
 
 @app.route('/campaignset/<campaignset_id>')
 def campaignset(campaignset_id):
-    campaignset = Campaign_Set.query.filter_by(campaign_set_id=campaignset_id)
+    campaignset = Campaign_Set.query.filter_by(campaign_set_id=campaignset_id).first()
     campaigns = Campaign.query.filter_by(campaign_set_id=campaignset_id).all()
-    return render_template('campaignset.html', campaignsets=campaignset, campaigns=campaigns)
+    return render_template('campaignset.html', campaignset=campaignset, campaigns=campaigns)
+
+@app.route('/campaignset/createcampaignset', methods=('GET', 'POST'))
+def campaignsetcreate():
+    return render_template('campaignsetcreate.html')
+
+@app.route('/campaign/<campaign_id>')
+def campaign(campaign_id):
+    campaign = Campaign.query.filter_by(Campaign_id=campaign_id).first()
+    adsets = Ad_Set.query.filter_by(Campaign_Id=campaign_id).all()
+    return render_template('campaign.html', campaign=campaign, adsets=adsets)
